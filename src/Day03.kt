159f8c7345d75c1afbaede6624b8668eb7e56891
Thoughtful Spring <xnh.developer@gmail.com>
2023-12-03 23:11:26 +0100

    feat: Day 03 part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -2,60 +2,60 @@ fun main() {
 
     fun Char.isSymbol(): Boolean = this != '.' && !isDigit()
 
-    fun parseData(input: List<String>): Int {
+    fun parseData(input: List<String>): Map<Pair<Int, Int>, List<Int>> {
         val rows = input.size
         val cols = input[0].lastIndex
-        val numbersFoundList: MutableList<Int> = mutableListOf()
+        // Map to store symbol coordinates and adjacent numbers
+        val symbolCoordinatesMap =  mutableMapOf<Pair<Int, Int>, MutableList<Int>>()
 
         for (ni in 0 until rows) {
             for (match in """\d+""".toRegex().findAll(input[ni])) {
+                val number = match.value.toInt()
                 val minRange = (match.range.first - 1).coerceAtLeast(0)
                 val maxRange = (match.range.last + 1).coerceAtMost(cols)
-                var addNumber = false
-
-                // Check above
                 val topLane = ni - 1
-                if (topLane > 0) {
-                    for (ti in minRange..maxRange) {
-                        if (input[topLane][ti].isSymbol()) {
-                            addNumber = true
-                        }
-                    }
-                }
-                // Check below
                 val botLane = ni + 1
-                if (botLane < rows) {
-                    for (ti in minRange..maxRange) {
-                        if (input[botLane][ti].isSymbol()) {
-                            addNumber = true
-                        }
-                    }
-                }
-                // Check sides
-                if (input[ni][minRange].isSymbol() || input[ni][maxRange].isSymbol()) {
-                    addNumber = true
-                }
 
-                if (addNumber) {
-                    numbersFoundList.add(match.value.toInt())
+                for (ti in minRange..maxRange) {
+                    // Check above
+                    if (topLane > 0 && input[topLane][ti].isSymbol()) {
+                        val symbolCoordinate = Pair(topLane, ti)
+                        symbolCoordinatesMap.computeIfAbsent(symbolCoordinate) { mutableListOf() }
+                        symbolCoordinatesMap[symbolCoordinate]?.add(number)
+                    }
+                    // Check below
+                    if (botLane < rows && input[botLane][ti].isSymbol()) {
+                        val symbolCoordinate = Pair(botLane, ti)
+                        symbolCoordinatesMap.computeIfAbsent(symbolCoordinate) { mutableListOf() }
+                        symbolCoordinatesMap[symbolCoordinate]?.add(number)
+                    }
+
+                    // Check sides
+                    if (input[ni][ti].isSymbol()) {
+                        val symbolCoordinate = Pair(ni, ti)
+                        symbolCoordinatesMap.computeIfAbsent(symbolCoordinate) { mutableListOf() }
+                        symbolCoordinatesMap[symbolCoordinate]?.add(number)
+                    }
                 }
             }
         }
-//        numbersFoundList.println()
-        return numbersFoundList.sumOf { it }
+        symbolCoordinatesMap.println()
+        return symbolCoordinatesMap
     }
-
 
     fun part1(input: List<String>): Int {
-        return parseData(input)
+        return parseData(input).values.sumOf { it.sum() }
     }
-
 
     fun part2(input: List<String>): Int {
-
-        return input.size
+        return parseData(input).values.sumOf {
+            if (it.size > 1) {
+                it.fold(1) { acc, value -> acc * value }.toInt()
+            } else {
+                0
+            }
+        }
     }
-
 
 // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
@@ -65,4 +65,3 @@ fun main() {
     part1(input).println()
     part2(input).println()
 }
-
